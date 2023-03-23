0891d2aae70d92bc1506a723285fcc604005a347
Viktor Derazhenko <vderazhenkov17@gmail.com>
2023-03-23 22:07:08 +0200

    homework 3

---
+++ homeWork3/homeWork3_1.js
@@ -0,0 +1,4 @@
+let firstWord = prompt('Enter first word')
+let secondWord = prompt('Enter second word')
+let thirdWord = prompt('Enter third word')
+alert('Your words: ' + firstWord + ', ' + secondWord + ', ' + thirdWord)
