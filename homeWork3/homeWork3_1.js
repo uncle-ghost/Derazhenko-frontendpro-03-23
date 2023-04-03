97d8552cc68cddfae44075bf1cc8f4b417b60caf
Viktor Derazhenko <vderazhenkov17@gmail.com>
2023-04-03 11:05:24 +0300

    renamed directories

--- homeWork3/homeWork3_1.js
+++
@@ -1,4 +0,0 @@
-let firstWord = prompt('Enter first word')
-let secondWord = prompt('Enter second word')
-let thirdWord = prompt('Enter third word')
-alert('Your words: ' + firstWord + ', ' + secondWord + ', ' + thirdWord)
--- homeWork3/homeWork3_2.js
+++
@@ -1,3 +0,0 @@
-let number = 37584
-let string = String(number).split('').join(' ')
-console.log(string)
